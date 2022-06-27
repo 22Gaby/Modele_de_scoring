25f21b3da5a75bd1d88ffe7b7fdcd85ef461b55c
Gabriela <gsandolache@yahoo.com>
2022-06-27 18:05:04 +0200

    all files- final version

--- API/app.py
+++ API/app.py
@@ -6,15 +6,17 @@ from flask import Flask, render_template, send_file, url_for
 from pathlib import Path
 
 
+
 origin_path=Path('./')
 ressources_path = origin_path.joinpath('ressources')
 modele_path = ressources_path.joinpath('modele')
 data_csv_path = ressources_path.joinpath('data_csv')
 
 ressources1_path = data_csv_path.joinpath('df_test_features_173_500clients.csv')
-modele1_path = modele_path.joinpath('model_best_lgbm.pickle')
+modele1_path = modele_path.joinpath('best_lgbm_model_cout_train.pickle')
+modele1_scaler_path = modele_path.joinpath('scaler.pickle')
 ressources2_path = data_csv_path.joinpath('df_info_pret_client_500clients.csv')
-
+ressources3_path = data_csv_path.joinpath('df_features_imp_dashboard.csv')
 
 df_test= pd.read_csv(ressources1_path)
 
@@ -23,6 +25,10 @@ id_clients = list(df_test['SK_ID_CURR'])
 with open(modele1_path, 'rb') as obj:
     modele_best = pickle.load(obj)
     print(modele_best)
+with open(modele1_scaler_path, 'rb') as obj:
+    scaler = pickle.load(obj)
+    print(scaler)
+
 
 app = Flask(__name__)
 
@@ -38,23 +44,23 @@ def calculate_score(id_client:int):
     :return: score client, score_classe
     '''
     df_id_client = df_test[df_test['SK_ID_CURR'] == int(id_client)]
-    X_test_id_client = df_id_client.drop('SK_ID_CURR', axis=1)
+    X_test_id_client = df_id_client.drop(['SK_ID_CURR','TARGET'], axis=1)
+    X_test_id_client = scaler.transform(X_test_id_client)
     y_proba_id_client = modele_best.predict_proba(X_test_id_client)[:, 1]
     score_id_client = int(round(y_proba_id_client[0] * 100, 2))
-    score_classe = modele_best.predict(X_test_id_client)[0]
+    mask_seuil_id_client = y_proba_id_client>=0.57
+    mask_seuil_id_client = mask_seuil_id_client.astype(int)
+    score_classe = mask_seuil_id_client[0]
     dic = {"score_client": float(score_id_client), "score_classe": str(score_classe)}
     # encodage du dic en objet json
     dic = json.dumps(dic)
     return dic
 
 
-
-
-
 @app.route("/csv_info_pret_client/")
 def csv_info_pret_client():
     '''
-    Définition route pour acceder aux infos clients
+    Définition route pour acceder aux infos et prêt des clients
     :return: file_csv
     '''
     return send_file(ressources2_path)
@@ -63,11 +69,20 @@ def csv_info_pret_client():
 @app.route("/csv_df_test/")
 def csv_df_test():
     '''
-    Définition route pour acceder au dataframe avec données de test pour le modele
+    Définition route pour acceder au dataframe avec les données de test pour le modèle
     :return: file_csv
     '''
     return send_file(ressources1_path)
 
+@app.route("/csv_df_features_imp/")
+def csv_df_features_imp():
+    '''
+    Définition route pour acceder au dataframe des graphiques features importance
+    :return: file_csv
+    '''
+    return send_file(ressources3_path)
+
 
 if __name__ == "__main__":
+    # connexion API on local (port 5000)
     app.run(debug=True)
--- Dashboard/dashboard.py
+++ Dashboard/dashboard.py
@@ -8,34 +8,41 @@ import pandas as pd
 import json
 from pathlib import Path
 
-# version ID_CLIENT statique
-ID_CLIENT = 103462
+# version ID_CLIENT statique (selection du ID du premier client)
+ID_CLIENT = 320753
 origin_path=Path('./')
 temp_path = origin_path.joinpath('temp')
 csv_info_pret_client_path = temp_path.joinpath('df_info_pret_client_500clients.csv')
 csv_test_df_path = temp_path.joinpath('df_test_features_173_500clients.csv')
+csv_features_imp_df_path = temp_path.joinpath('df_features_imp_dashboard.csv')
 
 
 r_csv_info_pret = requests.get('http://127.0.0.1:5000/csv_info_pret_client/')
 open(csv_info_pret_client_path, "wb").write(r_csv_info_pret.content)
 csv_info_pret_client = pd.read_csv(csv_info_pret_client_path)
 
+r_features_imp = requests.get('http://127.0.0.1:5000/csv_df_features_imp/')
+open(csv_features_imp_df_path, "wb").write(r_features_imp.content)
+csv_features_imp_df = pd.read_csv(csv_features_imp_df_path)
 
 r_test = requests.get('http://127.0.0.1:5000/csv_df_test/')
 open(csv_test_df_path, "wb").write(r_test.content)
 csv_test_df = pd.read_csv(csv_test_df_path)
-fig_features_fix = px.scatter(csv_test_df, x='PAYMENT_RATE', y='DAYS_EMPLOYED', size_max=8)
 
-chart2 = px.scatter(data_frame=csv_test_df,
-                    x=csv_test_df.columns[1],
-                    y=csv_test_df.columns[2],
-                    title=f"Scatter with features selection {csv_test_df.columns[2]} vs {csv_test_df.columns[1]}",
+chart2 = px.scatter(data_frame=csv_features_imp_df,
+                    x=csv_features_imp_df.columns[7],
+                    y=csv_features_imp_df.columns[9],
+                    color=csv_features_imp_df['SCORE_CLIENT'],
+                    title=f"Graphique analyse {csv_features_imp_df.columns[9]} vs {csv_features_imp_df.columns[7]}",
                     )
 
-chart3 = px.bar(data_frame=csv_test_df,
-               x="OBS_30_CNT_SOCIAL_CIRCLE",
-               y=csv_test_df.columns[2],
-               title=f"OBS_30_CNT_SOCIAL_CIRCLE vs {csv_test_df.columns[2]}",)
+chart3 = px.histogram(data_frame=csv_features_imp_df,
+                      x=csv_features_imp_df.columns[11],
+                      histnorm='percent',
+                      color=csv_features_imp_df['PRED_CLASSE'],
+                      barmode='overlay',
+                      title=f"Distribution de {csv_features_imp_df.columns[11]} selon les classes",
+                      )
 
 
 
@@ -47,7 +54,7 @@ ind= go.Indicator(
     # next value = score_client par rapport une proba_seuil de 47
     value = 30,
     mode = "gauge+number+delta",
-    delta = {'reference': 47,
+    delta = {'reference': 57,
              'increasing': {'color': 'Crimson'},
              'decreasing': {'color': 'Green'}},
     gauge = {'axis': {'range': [None, 100],
@@ -60,7 +67,7 @@ ind= go.Indicator(
                        {'range': [50.51, 75], 'color': 'Orange'},
                        {'range': [75, 100], 'color': 'Crimson'}],
              'threshold': {'line': {'color': 'white', 'width': 10},
-                            'thickness': 0.35, 'value': 47}})
+                            'thickness': 0.35, 'value': 57}})
 fig_jauge.add_trace(ind)
 
 fig_jauge.update_layout(paper_bgcolor='white',
@@ -75,21 +82,21 @@ app = Dash(__name__,  external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 dropdown1_scatter_test_df = dcc.Dropdown(
         id="dropdown1_scatter_test_df",
-        options=[{"value":feature, "label":feature} for feature in csv_test_df.columns[1:]],
-        value=csv_test_df.columns[1],
+        options=[{"value":feature, "label":feature} for feature in csv_features_imp_df.drop(['Sexe', 'Niveau éducation', 'Statut familial'], axis=1).columns[4:]],
+        value=csv_features_imp_df.columns[7],
         clearable = False )
 
 
 dropdown2_scatter_test_df = dcc.Dropdown(
         id="dropdown2_scatter_test_df",
-        options=[{"value":feature, "label":feature} for feature in csv_test_df.columns[1:]],
-        value=csv_test_df.columns[2],
+        options=[{"value":feature, "label":feature} for feature in csv_features_imp_df.drop(['Sexe', 'Niveau éducation', 'Statut familial'], axis=1).columns[5:]],
+        value=csv_features_imp_df.columns[9],
         clearable = False )
 
 dropdown_bar_chart3 = dcc.Dropdown(
         id="dropdown_bar_chart3",
-        options=[{"value":label, "label":label} for label in csv_test_df.columns[1:]],
-        value=csv_test_df.columns[1],
+        options=[{"value":label, "label":label} for label in csv_features_imp_df.drop(['Sexe', 'Niveau éducation', 'Statut familial'], axis=1).columns[4:]],
+        value=csv_features_imp_df.columns[11],
         clearable = False )
 
 
@@ -110,7 +117,7 @@ row0 = html.Div(children=[
     html.Img(src=app.get_asset_url('pandas_logo.png'), style={"height": "40px", "margin-left": "30px", }),
 ])
 
-header = html.H2(children="Welcome to 👨 Gabriela's Dashboard ")
+header = html.H3(children="Welcome to 👨 Gabriela's Dashboard ")
 
 
 # creation cards for row1: card_logo_row1 pour menu de gauche
@@ -123,16 +130,17 @@ card_logo_row1 = dbc.Card(
 card_main_row1 = dbc.Card(
     [
         html.Div(children=[
-            html.H2("🏠 Projet 7: Implémentez un modèle de scoring"),
+            html.H1("🏠 Projet 7: Implémentez un modèle de scoring"),
+            html.Br(),
             dcc.Markdown(
-                '''L’entreprise intitulé **Prêt à dépenser**, souhaite mettre en œuvre un **outil de “scoring crédit”** pour calculer la probabilité qu’un client rembourse son crédit, 
+                '''L’entreprise intitulé **"Prêt à dépenser"**, souhaite mettre en œuvre un **outil de “scoring crédit”** pour calculer la probabilité qu’un client rembourse son crédit, 
                 puis classifie la demande en crédit accordé ou refusé. 
                 L’entreprise souhaite développer un **algorithme de classification** en s’appuyant sur des sources de données variées 
                 (données comportementales, données provenant d'autres institutions financières, etc.)
                Prêt à dépenser décide de développer un **dashboard interactif**  
                facilement exploitable par les chargés de relation client afin d’expliquer les décisions d’octroi de crédit, 
                et de disposer des informations clients à explorer facilement.''',
-                style={'white-space': 'pre', 'fontSize': 20}, ),
+                style={'white-space': 'pre', 'fontSize': 22}, ),
         ], ),
     ],
     color="#cee1eb", outline=True,
@@ -143,15 +151,16 @@ card_main_row1 = dbc.Card(
 card_main_row2 = dbc.Card(
     [
          dbc.CardBody([
-                html.H4("🔎 Selection ID Client",  className="card-subtitle"),
+             html.H3("API de prédiction solvabilité",  className="card-subtitle"),
+             html.Br(),
+             html.Br(),
+                html.H4("🔎 Tapez ID Client pour modélisation prédiction",  className="card-subtitle"),
                 html.Br(),
-                dcc.Dropdown(csv_test_df.SK_ID_CURR, id='id_client_input',  placeholder="Select Client Principal",
+                dcc.Dropdown(csv_test_df.SK_ID_CURR, id='id_client_input',  placeholder="Select ID Client pour PREDICTION",
                  ),
                 html.Br(),
                 html.Br(),
-                html.Br(),
-                html.Br(),
-                html.H4("📉 Jauge Score Crédit ID Client", className="card-subtitle", style={"background-color": "pink",}),
+                html.H4("📉 Jauge du Score de prédiction", className="card-subtitle", style={"background-color": "pink",}),
                 html.Br(),
                 dcc.Graph(figure=fig_jauge, id="my_jauge", )],
              style={"border-style": "groove","margin-left":"auto", "margin-right":"auto",
@@ -164,18 +173,17 @@ card_main_row2 = dbc.Card(
 # creation cards for row2: card_question_row2 pour menu de droite
 card_question_row2 = dbc.Card([
     dbc.CardBody([
-        html.H3('👁️ Client : Informations descriptives et demande de prêt :',
-            style={"background-color": "pink", }),
-        html.H4('Selection du Client Principal:', style={"text-align": "left", "font-family": "Times New Roman"}),
-        dcc.Dropdown(csv_info_pret_client.SK_ID_CURR, id='id_client_info_input',
-             placeholder="Select Client Principal", style={"width": "40%"}),
+        html.H3('👁️ Client Principal: Informations descriptives et demande de prêt :',
+                ),
+        html.Br(),
         html.H4('Données personnelles du Client :', ),
         dash_table.DataTable(csv_info_pret_client.to_dict('records'),
                              [{"name": i, "id": i} for i in csv_info_pret_client.columns[1:9]],
                              style_header={'backgroundColor': '#cee1eb', "font-family": "Times New Roman",'fontSize': 20, },
                             style_cell={'textAlign': 'center'},
                             style_data={'backgroundColor': '#cee1eb', "font-family": "Times New Roman", 'fontSize': 18,},
-                            id='table1_info_client',),
+                            id='table1_info_client',
+                                     ),
         html.Br(),
         html.H4('Données de la demande du crédit :', ),
         dash_table.DataTable(csv_info_pret_client.to_dict('records'),
@@ -183,20 +191,39 @@ card_question_row2 = dbc.Card([
                              style_header={'backgroundColor': '#cee1eb', "font-family": "Times New Roman", 'fontSize': 20,},
                             style_cell={'textAlign': 'center'},
                             style_data={'backgroundColor': '#cee1eb', "font-family": "Times New Roman", 'fontSize': 18, },
-                             id='table2_info_client',),
+                             id='table2_info_client',
+                                     ),
         html.Br(),
-        html.H3('📉 Prediction Solvabilité du Client:', style={"background-color": "pink", }),
+        html.H3('📉 Résultat de Prediction de la Solvabilité du Client Principal:',
+                ),
         dcc.Markdown(
                 "La prédiction de la probabilité que le Client rembourse le crédit est calculé "
                 "via une API spécifique avec LGBMClassifier, le meilleur modèle optimisé."
-                "Pour le Client  {id_client} le score calculé via l’API est :  **{score_client}** \n "
-                "L’algorithme de classification indique que la solvabilité du client est **{score_classe}** \n "
+                "Pour le Client  {id_client} la probabilité calculée via l’API est :  **{score_client}** \n "
+                "L’algorithme de classification indique une solvabilité du client de **{score_classe}** \n "
                 "Le prêt est accordé au Client : **{score_text}**",
                 style={'white-space': 'pre', 'fontSize': 20, "font-family": "Times New Roman", },
                 id='id_client_output_2',),
         ]),
     ], color="#cee1eb", )
 
+
+# création row3:
+header_row3 = html.H3(children="Visualisation des caractéristiques importantes pour la décision d’octroi de crédit ",
+                      style={"background-color": "pink", "margin-left": 200, "margin-right": 200, "text-align": "center", })
+
+text_buton = html.H5(children="Tapez ID pour visualisation position du Client Principal vis-à-vis des autres clients: ",
+                      style={ "margin-left": 300, "margin-right": 500, "text-align": "center", })
+
+button_input =html.Div(children=[
+    dcc.Dropdown(csv_features_imp_df.SK_ID_CURR,
+        placeholder='Entrer ID client pour visualisation',
+        id='input_box_features',
+        style={'textAlign': 'center', "margin-left": 200, "margin-right": 500,},
+    )])
+
+text_graphiques = html.H4(children="Graphiques de positionnement du Client Principal vis-à-vis des autres clients sur les caractéristiques importantes: ",
+                      style={ "margin-left": 300, "margin-right": 500, "text-align": "center", })
 
 scatter_div= html.Div(children=[
     dropdown1_scatter_test_df, dropdown2_scatter_test_df, html.Br(),graph2 ],
@@ -206,7 +233,9 @@ bar_div = html.Div(children=[dropdown_bar_chart3, html.Br(),graph3],
                    style={'display': 'inline-block',"width": "40%",
                           "margin-left": "70px", "background-color": "#cee1eb",} )
 
-row3= html.Div(children=[scatter_div, bar_div, html.Br(), ],)
+row3= html.Div(children=[header_row3, html.Br(),  text_buton, button_input, html.Br(), html.Br(), text_graphiques, html.Br(), scatter_div, bar_div, html.Br(), ],)
+
+
 
 
 # definition layout
@@ -215,6 +244,22 @@ layout = html.Div(children=[
     dbc.Row([dbc.Col(card_logo_row1, width={'size': 2, 'offset': 1}, ),
              dbc.Col(card_main_row1, width={'size': 9, 'offset': 0}),],),
     html.Hr(),
+
+
+    html.H3(children="Les CLIENTS de la société - Prêt à dépenser -",
+                      style={"background-color": "pink", "margin-left": 200, "margin-right": 200,
+                             "text-align": "center"}),
+    html.Br(),
+    html.H4('Sélectionner l’identifiant ID du Client Principal:', style={ "font-family": "Times New Roman",
+                                                      "margin-left":10,
+                                                      }),
+    dcc.Dropdown(csv_info_pret_client.SK_ID_CURR, id='id_client_info_input',
+                 placeholder="Select ID du Client Principal", style={"width": "60%",
+                                                               "margin-left":400,
+                                                               }),
+    html.Br(),
+    html.Br(),
+
     dbc.Row([dbc.Col(card_main_row2, width={'size': 2, 'offset': 1, },),
              dbc.Col(card_question_row2, width=8),], justify="around"),
     html.Hr(),
@@ -244,7 +289,7 @@ def update_gauge(id_client):
     return fig_jauge
 
 
-# callback: retour de prediction score client en affichage text
+# callback: retour de prediction API score client en affichage text et score
 @app.callback(
     Output(component_id='id_client_output_2', component_property='children'),
     Input(component_id='id_client_input', component_property='value'))
@@ -255,17 +300,19 @@ def search_info_client_dropdown(id_client):
     score_client = content_score_client["score_client"]
     score_classe = content_score_client["score_classe"]
     if score_classe == "0":
-        score_text = "NON"
+        score_text = "OUI"
     else:
-        score_text = "Oui"
+        score_text = "NON"
     return f'''
-            La prédiction de la probabilité que le Client rembourse le crédit est calculé via une API spécifique avec LGBMClassifier, le meilleur modèle optimisé. 
-            Pour le Client  {id_client} le score calculé via l’API est :  **{score_client}**
-            L’algorithme de classification indique que la solvabilité du client est **{score_classe}**
-            Le prêt est accordé au Client : **{score_text}**'''
+            La prédiction de la probabilité que le Client Principal rembourse le crédit est calculé via une API spécifique.
+            Le meilleur modéle choisi est LGBMClassifier, avec les hyperparamètres optimisés. 
+            Pour le Client  {id_client} la probabilité calculé via l’API est de :  **{score_client}%**
+            Plus la probabilité est faible, plus le client est solvable. Le seuil maximum de solvabilité est de 57%.
+            L’algorithme de classification indique le positionnement du client dans la classe **{score_classe}**
+            La demande de crédit est accordée au Client Principal: **{score_text}**'''
 
 
-# callback: bouton id_client - 2 tables de 1 ligne avec info cleint et prêt
+# callback: bouton id_client - 2 tables: 1 ligne avec info client et 1 ligne avec prêt
 @app.callback(
     [Output('table1_info_client', 'data'),
     Output('table2_info_client', 'data')],
@@ -282,33 +329,63 @@ def update_info_client(id_client):
     return row09.to_dict("records"), row014.to_dict("records")
 
 
+# callback: features pour graphique bi-variées (gauche)
 @app.callback(
     Output('graph2', 'figure'),
     [Input('dropdown1_scatter_test_df', 'value'),
-     Input('dropdown2_scatter_test_df', 'value')])
-def update_scatter(drop1, drop2):
-    chart2 = px.scatter(data_frame=csv_test_df,
-                   x=drop1,
-                   y=drop2,
-                   title=f"Scatter with features selection {drop2} vs {drop1}",
-                   height=500,)
+     Input('dropdown2_scatter_test_df', 'value'),
+     Input('input_box_features', 'value'),
+        ])
+def update_scatter(drop1, drop2, id_client):
+    if id_client is None:
+        id_client = ID_CLIENT
+    csv_features_imp_df = pd.read_csv(csv_features_imp_df_path)
+    csv_features_imp_df = csv_features_imp_df.set_index("SK_ID_CURR")
+    chart2 = px.scatter(data_frame=csv_features_imp_df,
+                        x=drop1,
+                        y=drop2,
+                        color=csv_features_imp_df['SCORE_CLIENT'],
+                        title=f"Graphique d'analyse {drop2} versus {drop1}",
+                        height=500, )
+    chart2.add_vline(x=csv_features_imp_df[drop1][id_client],
+                     line_width=3, line_dash="dash", line_color="black",
+                     annotation_text=f"Client {id_client}", annotation_font_size=14, annotation_font_color="black")
+    chart2.add_hline(y=csv_features_imp_df[drop2][id_client],
+                     line_width=3, line_dash="dash", line_color="black",
+                     annotation_text=f"Client {id_client}", annotation_font_size=14, annotation_font_color="black")
     return chart2
 
 
+# callback: features pour graphique histogram (droite)
 @app.callback(
     Output('graph3', 'figure'),
-    [Input('dropdown_bar_chart3', 'value')])
-def update_bar(bar_drop):
-    chart3 = px.bar(data_frame=csv_test_df,
-               x="OBS_30_CNT_SOCIAL_CIRCLE",
-               y=bar_drop,
-               title=f"OBS_30_CNT_SOCIAL_CIRCLE vs {csv_test_df.columns[2]}",
-               height=500,
-               )
+    [Input('dropdown_bar_chart3', 'value'),
+     Input('input_box_features', 'value'),
+     ])
+def update_bar(bar_drop, id_client):
+    if id_client is None:
+        id_client = ID_CLIENT
+    csv_features_imp_df = pd.read_csv(csv_features_imp_df_path)
+    csv_features_imp_df = csv_features_imp_df.drop(['Sexe', 'Niveau éducation', 'Statut familial'], axis=1)
+    csv_features_imp_df = csv_features_imp_df.set_index("SK_ID_CURR")
+    chart3 = px.histogram(data_frame=csv_features_imp_df,
+                          x=bar_drop,
+                          histnorm='percent',
+                          color=csv_features_imp_df['PRED_CLASSE'],
+                          barmode='overlay',
+                          title=f"Distribution de {bar_drop} selon les classes",
+                          height=500,
+                          )
+    chart3.add_vline(x=csv_features_imp_df[bar_drop][id_client],
+                     line_width=3, line_dash="dash", line_color="black",
+                    annotation_text=f"Client {id_client}", annotation_font_size=14, annotation_font_color="black"
+                     )
     return chart3
 
 
 if __name__ == '__main__':
+    # connexion dashboard sur OVH, port internet=80
+#    app.run_server(host="162.19.76.116", port=80)
+    # connexion dashboard on local
     app.run_server(debug=True)
-# port=80
 
